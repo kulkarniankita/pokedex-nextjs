db971a30bebd863cdd2fe3799f1da7ddf469b8f9
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2025-02-13 18:01:46 -0500

    Building Pokemon Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import PokemonCard from '@/components/pokemon-card';
+
 async function getPokemonData() {
   const url = 'https://pokeapi.co/api/v2/pokemon?limit=20';
   try {
@@ -6,8 +8,24 @@ async function getPokemonData() {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-    return json.results;
+    const data = await response.json();
+
+    const pokemonDetails = await Promise.all(
+      data.results.map(async (pokemon: { url: string }) => {
+        const res = await fetch(pokemon.url);
+        const details = await res.json();
+
+        return {
+          id: details.id,
+          name: details.name,
+          types: details.types.map(
+            (type: { type: { name: string } }) => type.type.name
+          ),
+          imageUrl: details.sprites.other['official-artwork'].front_default,
+        };
+      })
+    );
+    return pokemonDetails;
   } catch (error) {
     console.error(error.message);
     return [];
@@ -18,12 +36,16 @@ export default async function Home() {
   const pokemons = await getPokemonData();
 
   return (
-    <div className="">
-      Pokédex
-      <div className="grid grid-cols-3 gap-12">
-        {pokemons.map((pokemon, idx) => (
-          <p key={`${pokemon.name}-${idx}`}>{pokemon.name}</p>
-        ))}
+    <div className="min-h-screen bg-gradient-to-b from-red-500 to-red-600">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl md:text-6xl font-bold text-center text-white mb-8">
+          Pokédex
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
+          {pokemons.map((pokemon, idx) => (
+            <PokemonCard key={`${pokemon.name}-${idx}`} pokemon={pokemon} />
+          ))}
+        </div>
       </div>
     </div>
   );
